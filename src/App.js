22affa5a8ea75fda20cffc7fffe49e7420f4826e
yngvi@merkurpoint.is <yngvi@merkurpoint.is>
2022-10-04 13:26:49 +0200

    pic and text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
+import YngviPic from './IMG_4964.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={YngviPic} className="App-logo" alt="logo" />
         <p>
-          This is the homepage of YngviThor
+          This is the homepage of Yngvi Thor
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
